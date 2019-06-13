db0b5ff084049a3583ba16107eee2a6a575a1939
Justin Orefice <justingorefice@gmail.com>
2019-06-12 23:40:43 -0400

    Rebuild for particle movement between elements bug fixing

--- core/src/CabanaM.hpp
+++ core/src/CabanaM.hpp
@@ -60,6 +60,42 @@ class CabanaM
     KOKKOS_FUNCTION
     AoSoA_t* aosoa() { return _aosoa; }
 
+    void rebuild(int* new_parent, int num_new_ptcls, int* new_ptcl_parents)//, 
+//SOA* new_ptcl)
+    {
+      int SIMD_WIDTH = vector_length();
+      int numSoA = this->aosoa()->numSoA();
+      int sizes[numSoA] = {0};
+      int newOffsets[offset(size())] = {0};
+      //first loop to count number of particles per element (atomic)
+      //for (int i =0; i < size(); i++){
+      Kokkos::parallel_for("particlesPerElement", size(), KOKKOS_LAMBDA(const int& i){
+          Kokkos::atomic_increment(&sizes[offset(i)]); //use mask instead?
+      )};
+     // }
+      CabanaM newCabanaM(sizes, size() + num_new_ptcls);
+      //fill by usig offset array to find (in the old cabana)  all the particles for an element, then store them next to each other in the new cabana
+      Kokkos::parallel_scan(this->sizes.size(), newOffsets, KOKKOS_LAMBDA( const int& i, int& upd, const bool& last) {
+        const int val_i = offset(i);
+        if (last){
+	 newCabanaM._offsets[i] = upd;
+        } 
+	upd+= val_i;
+      });
+      Kokkos::parallel_for("BtoA", numSoA, KOKKOS_LAMBDA(int a){
+      //for (int a = 0; a < numSoA; a++){
+	int b = offset(a); //mask?
+	auto first = newCabanaM.offset(b);
+	auto j = Kokkos::atomic_fetch_add<int>(&sizes[offset(a)], 1);
+	Kokkos::parallel_for("collect", offset(a+1) - offset(a), KOKKOS_LAMBDA(a){
+          auto tp= aosoa().getTuple(a);
+          newCabanaM.aosoa()->setTuple(a,tp);
+        )};
+      )};
+    //}
+    //free(this);
+    }
+
   private:
     std::size_t _size; // size of offset array
     std::size_t _vector_length;
